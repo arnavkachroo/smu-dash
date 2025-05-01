1e6e490ac1bf9bb8512c2bcc676d23b66fe58428
arnavkachroo <arnavkachroo@gmail.com>
2025-05-01 18:40:26 -0400

    Final

--- app/submiteval/[id]/page.tsx
+++ app/submiteval/[id]/page.tsx
@@ -6,27 +6,30 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-export default function EvaluationPage({ params }: { params: { id: string } }) {
+const EvaluationPage = (props: any) => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { data: session, status } = useSession();
-    const router = useRouter();
+  const { params } = props as { params: { id: string } };
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
-    useEffect(() => {
-        if (status === "unauthenticated") {
-            router.push("/login");
-        }
-    }, [status, router]);
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login");
+    }
+  }, [status, router]);
 
-    if (status === "loading") return <p>Loading...</p>;
-    if (status === "unauthenticated") return null;
+  if (status === "loading") return <p>Loading...</p>;
+  if (status === "unauthenticated") return null;
 
-    return (
-        <div>
-            <Navbar />
-            <br />
-            <br />
-            <br />
-            <SubmitEval params={params} />
-        </div>
-    );
-}
+  return (
+    <div>
+      <Navbar />
+      <br />
+      <br />
+      <br />
+      <SubmitEval params={params} />
+    </div>
+  );
+};
+
+export default EvaluationPage;
